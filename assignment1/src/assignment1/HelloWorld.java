f0141a3fc30563f566ac08a19af36062d8011129
ablera <ablera@b51fa056-ba0b-4c6c-ae0e-4dee61bb4688>
2015-02-19 13:33:28 +0000

    Still nothing.

--- assignment1/src/assignment1/HelloWorld.java
+++ assignment1/src/assignment1/HelloWorld.java
@@ -3,7 +3,7 @@ package assignment1;
 public class HelloWorld {
 	
 	public static void main (String[] args) {
-		System.out.println("ERROR 404 world not found.");
+		System.out.println("ERROR 404 world still not found.");
 	}
 
 }
