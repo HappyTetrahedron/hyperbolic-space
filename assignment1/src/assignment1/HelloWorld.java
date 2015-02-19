7598f1b0ea207c3b838eba177d3d7571b6edb583
ablera <ablera@b51fa056-ba0b-4c6c-ae0e-4dee61bb4688>
2015-02-19 13:23:09 +0000

    Now it's gone...

--- assignment1/src/assignment1/HelloWorld.java
+++ assignment1/src/assignment1/HelloWorld.java
@@ -3,7 +3,7 @@ package assignment1;
 public class HelloWorld {
 	
 	public static void main (String[] args) {
-		System.out.println("Yep, world is still here.");
+		System.out.println("ERROR 404 world not found.");
 	}
 
 }
